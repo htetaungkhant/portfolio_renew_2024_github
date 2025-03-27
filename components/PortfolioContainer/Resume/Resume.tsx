dc4964ce7e26c8e6feff0ecddcd4531b4bef3c09
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-03-28 04:41:23 +0700

    Changed "Programming Skills" to only "Skills"

--- components/PortfolioContainer/Resume/Resume.tsx
+++ components/PortfolioContainer/Resume/Resume.tsx
@@ -46,7 +46,7 @@ const Resume = (props: { id: string; screenName?: string }) => {
     // { label: "Projects", logoSrc: "projects.svg" },
     { label: "Education", logoSrc: "education.svg" },
     { label: "Work History", logoSrc: "work-history.svg" },
-    { label: "Programming Skills", logoSrc: "programming-skills.svg" },
+    { label: "Skills", logoSrc: "programming-skills.svg" },
     { label: "Abilities", logoSrc: "abilities.svg" },
     { label: "Interests", logoSrc: "interests.svg" },
   ];
