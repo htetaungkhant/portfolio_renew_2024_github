476c5e05ad27e56621a7d48320266d5b2fb656a6
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-04-09 22:42:14 +0700

    minor data description fix

--- data/AboutMe/constants.ts
+++ data/AboutMe/constants.ts
@@ -2,7 +2,7 @@ export const CONSTANTS = {
     introduction: {
         title: "Who I Am",
         description:
-            "A Frontend-Focused Full-Stack Developer with solid experience in React.js, Next.js, React Native, and TypeScript. Passionate about building scalable and user-friendly applications while continuously improving performance and maintainability. I aim to contribute my skills to impactful projects, collaborate with teams effectively, and stay updated with modern web technologies to deliver high-quality solutions.",
+            "A Frontend-Focused Full-Stack Developer with solid experience in React.js, Next.js, React Native, and Express.js. Passionate about building scalable and user-friendly applications while continuously improving performance and maintainability. I aim to contribute my skills to impactful projects, collaborate with teams effectively, and stay updated with modern web technologies to deliver high-quality solutions.",
     },
     highlights: {
         heading: "Here are a Few Highlights:",
