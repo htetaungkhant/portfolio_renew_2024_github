573edbea99c755d5f68af7addbb25de809eefb93
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-04-11 14:08:53 +0700

    updated Typed text of Profile section

--- data/Home/constants.ts
+++ data/Home/constants.ts
@@ -2,7 +2,7 @@ export const headerTitle = "Htet Khant";
 
 export const profileDetails = {
     name: "Htet Aung Khant",
-    titles: ["Enthusiastic Dev", "Full stack Developer", "MERN stack Dev", "Cross Platform Dev", "React / Next.js / React Native"],
+    titles: ["Full stack Developer", "MERN stack Dev", "Cross Platform Dev", "React / Next.js Dev", "React Native Dev"],
     description:
         "Knack of building applications with front and back end operations.",
 };
