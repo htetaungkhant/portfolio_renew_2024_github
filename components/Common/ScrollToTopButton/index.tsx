dac6c48fdbcefcc9cf1638c188f2ec0ce4b9d9f9
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-04-12 15:48:15 +0700

    added "backToRootUrl" prop at ScrollToTopButton component

--- components/Common/ScrollToTopButton/index.tsx
+++ components/Common/ScrollToTopButton/index.tsx
@@ -1,15 +1,29 @@
 "use client";
 
+import { useRouter } from 'next/navigation';
+
 import { scrollToTop } from "@/lib/utils";
 
 import classes from "./index.module.scss";
 
-export default function ScrollToTopButton() {
+interface ScrollToTopButtonProps {
+  backToRootUrl?: boolean;
+}
+
+export default function ScrollToTopButton({ backToRootUrl = false }: ScrollToTopButtonProps) {
+  const router = useRouter();
+  const handleClick = () => {
+    if (backToRootUrl) {
+      router.push("/", { scroll: false });
+    }
+    scrollToTop();
+  };
+
   return (
     <div className={classes["scroll-container"]}>
       <button
         className={classes["btn-scroll"]}
-        onClick={scrollToTop}
+        onClick={handleClick}
       >
         <i className="fa fa-arrow-up"></i>
       </button>
